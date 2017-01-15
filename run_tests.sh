c86d742f6c2f04b15c39fe56d3f3583dc8d0afd9
antihax@gmail.com <antihax@gmail.com>
2017-01-14 21:31:25 -0600

    remove tests due to changes in API.

--- run_tests.sh
+++
@@ -1,5 +0,0 @@
-java -jar swagger-codegen-cli.jar generate -o ./tests/go-petstore -t ./template -l go -i ./tests/petstore-with-fake-endpoints-models-for-testing.yaml
-go run tools/postgen.go -src ./tests/go-petstore
-cd ./tests
-go get
-go test
