09efa49d5563fddb2451b6ef73a053256f052b2e
antihax@gmail.com <antihax@gmail.com>
2017-02-02 19:53:09 -0600

    Move to one build script for new repo.

--- build_esi_evedata.sh
+++
@@ -1,7 +0,0 @@
-cls
-COUNTER=1
-while [  $COUNTER -lt 5 ]; do
-    rm ../esi/v$COUNTER/*
-    java -jar swagger-codegen-cli.jar generate -o ../esi/v$COUNTER -t ./template -l go -i https://esi.tech.ccp.is/v$COUNTER/swagger.json?datasource=tranquility -DpackageName=esiv$COUNTER
-    let COUNTER=COUNTER+1 
-done
