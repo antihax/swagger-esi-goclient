97f4c2c0328e9596d0829d891064fa3f1c183e22
antihax@gmail.com <antihax@gmail.com>
2017-02-02 19:43:16 -0600

    Update

--- build_esi_evedata.sh
+++ build_esi_evedata.sh
@@ -1,3 +1,7 @@
 cls
-rm ../esi/*
-java -jar swagger-codegen-cli.jar generate -o ../esi -t ./template -l go -i https://esi.tech.ccp.is/latest/swagger.json?datasource=tranquility -DpackageName=esi
+COUNTER=1
+while [  $COUNTER -lt 5 ]; do
+    rm ../esi/v$COUNTER/*
+    java -jar swagger-codegen-cli.jar generate -o ../esi/v$COUNTER -t ./template -l go -i https://esi.tech.ccp.is/v$COUNTER/swagger.json?datasource=tranquility -DpackageName=esiv$COUNTER
+    let COUNTER=COUNTER+1 
+done
