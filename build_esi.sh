bccff22adb688e02a2e6bb372dea52a9f2b55641
antihax@gmail.com <antihax@gmail.com>
2017-02-06 17:43:40 -0600

    add mock-esi

--- build_esi.sh
+++ build_esi.sh
@@ -2,15 +2,31 @@
 
 rev=$(git rev-parse --short HEAD)
 
-cd ..
-mkdir goesi
-cd goesi
-git init
 git config --global user.name "${GH_USER}"
 git config --global user.email "${GH_EMAIL}"
 git config --global github.token "${GH_TOKEN}"
 git config --global push.default simple
 
+cd ..
+mkdir mock-esi
+cd mock-esi
+git init
+git remote add upstream "https://${GH_TOKEN}@github.com/antihax/mock-esi.git"
+git fetch upstream
+git pull upstream HEAD
+touch version.txt
+
+oldnum=`cut -d ',' -f2 version.txt`  
+newnum=`expr $oldnum + 1`
+sed -i "s/$oldnum\$/$newnum/g" version.txt 
+git commit -m "rebuild esi at ${rev}"
+git push -q upstream HEAD
+
+
+cd ..
+mkdir goesi
+cd goesi
+git init
 git remote add upstream "https://${GH_TOKEN}@github.com/antihax/goesi.git"
 git fetch upstream
 git pull upstream HEAD
