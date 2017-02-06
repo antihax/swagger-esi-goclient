35affd3b7e2507dfe25cdc1d66be7cf6fb428da9
antihax@gmail.com <antihax@gmail.com>
2017-02-06 16:45:16 -0600

    start travis build

--- build_esi.sh
+++ build_esi.sh
@@ -1,8 +1,34 @@
-cls
+#!/bin/bash
+
+rev=$(git rev-parse --short HEAD)
+
+cd ..
+mkdir goesi
+cd goesi
+git init
+git config --global user.name "${GH_USER}"
+git config --global user.email "${GH_EMAIL}"
+git config --global github.token "${GH_TOKEN}"
+git config --global push.default simple
+
+git remote add upstream "https://${GH_TOKEN}@github.com/antihax/goesi.git"
+git fetch upstream
+git pull upstream HEAD
+go get -v
+
 COUNTER=1
-while [  $COUNTER -lt 5 ]; do
-    rm ../esi/v$COUNTER/*
-    rm ../esi/v$COUNTER/doc/*
-    java -jar swagger-codegen-cli.jar generate -o ../goesi/v$COUNTER -t ./template -l go -i https://esi.tech.ccp.is/v$COUNTER/swagger.json?datasource=tranquility -DpackageName=goesiv$COUNTER
+while [ $COUNTER -lt 5 ]; do
+    rm ./v$COUNTER/*
+    rm ./v$COUNTER/doc/*
+    set -e
+    java -jar ../swagger-esi-goclient/swagger-codegen-cli.jar generate -o ./v$COUNTER -t ../swagger-esi-goclient/template -l go -i https://esi.tech.ccp.is/v$COUNTER/swagger.json?datasource=tranquility -DpackageName=goesiv$COUNTER
     let COUNTER=COUNTER+1 
+    set +e
 done
+
+set -e
+
+go test ./...
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD
