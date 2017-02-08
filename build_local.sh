842c4317a57abd26816b5a6240d3254064ab6aa6
antihax@gmail.com <antihax@gmail.com>
2017-02-07 18:44:08 -0600

    Update

---
+++ build_local.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+COUNTER=1
+
+while [ $COUNTER -lt 5 ]; do
+    rm ../goesi/v$COUNTER/*
+    rm ../goesi/v$COUNTER/docs/*
+    set -e
+    java -jar ./swagger-codegen-cli.jar generate -o ../goesi/v$COUNTER -t ./template -l go -i https://esi.tech.ccp.is/v$COUNTER/swagger.json?datasource=tranquility -DpackageName=goesiv$COUNTER
+    let COUNTER=COUNTER+1 
+    set +e
+done
+
+go test ./...
