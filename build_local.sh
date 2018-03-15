5e32c980628a6789edcab150f89da8dfea3c8d6b
antihax <antihax@gmail.com>
2018-03-15 14:41:24 -0700

    update to new host.

--- build_local.sh
+++ build_local.sh
@@ -4,10 +4,10 @@ rm -rf ../goesi/esi/*
 rm -rf ../goesi/esi/docs/*
 set -e
 # Generate models first and JSON code second because there is no easy way to glob for model files only
-java -jar -Dmodels ./swagger-codegen-cli.jar generate -o ../goesi/esi -t ./template -l go -i https://esi.tech.ccp.is/_latest/swagger.json?datasource=tranquility -DpackageName=esi
+java -jar -Dmodels ./swagger-codegen-cli.jar generate -o ../goesi/esi -t ./template -l go -i https://esi.evetech.net/_latest/swagger.json?datasource=tranquility -DpackageName=esi
 find ../goesi/esi/ -type f -name "*.go" -exec echo processing {} \; -exec easyjson -noformat {} \;
 # Generate all the other files
-java -jar ./swagger-codegen-cli.jar generate -o ../goesi/esi -t ./template -l go -i https://esi.tech.ccp.is/_latest/swagger.json?datasource=tranquility -DpackageName=esi
+java -jar ./swagger-codegen-cli.jar generate -o ../goesi/esi -t ./template -l go -i https://esi.evetech.net/_latest/swagger.json?datasource=tranquility -DpackageName=esi
 # Fix slices of struct types
 sed -i 's/REMOVEME\[\]//g' ../goesi/esi/*.*
 # Fix imports where needed (select encoding/json or easyjson)
--- dumpOpsMods.sh
+++ dumpOpsMods.sh
@@ -1,3 +1,3 @@
 cls
-java -DdebugModels -jar swagger-codegen-cli.jar generate -o ./dump -t ./template -l go -i https://esi.tech.ccp.is/latest/swagger.json?datasource=tranquility > mods.txt
-java -DdebugOperations -jar swagger-codegen-cli.jar generate -o ./dump -t ./template -l go -i https://esi.tech.ccp.is/latest/swagger.json?datasource=tranquility > ops.txt
+java -DdebugModels -jar swagger-codegen-cli.jar generate -o ./dump -t ./template -l go -i https://esi.evetech.net/latest/swagger.json?datasource=tranquility > mods.txt
+java -DdebugOperations -jar swagger-codegen-cli.jar generate -o ./dump -t ./template -l go -i https://esi.evetech.net/latest/swagger.json?datasource=tranquility > ops.txt
